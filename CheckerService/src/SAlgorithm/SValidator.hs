6b24c2273fda4616843b1d87a043bb09bdfdcf25
Stanisław Wasik <st.wasik@gmail.com>
2019-03-27 23:05:05 +0100

    Added function that carry out check process

--- CheckerService/src/SAlgorithm/SValidator.hs
+++ CheckerService/src/SAlgorithm/SValidator.hs
@@ -1,38 +1,60 @@
-module SAlgorithm.SValidator(validWords, allWords, sWords) where
+module SAlgorithm.SValidator where
 
 import SData.SChar
 import Data.Matrix as Mx 
 import Data.Tuple
 import Data.List as L
 import qualified Data.Set as S
+import SData.SBoard as SB
+import Debug.Trace as Dg
 
-updatePartOfInvalid :: [SWord] -> [SWord]
-updatePartOfInvalid words = map mapPart words
-    where mapPart = map updateIfValid
-          updateIfValid (SChar c Valid p) = SChar c PartOfInvalid p 
-          updateIfValid schar = schar
-    
-allPartOfInvalid :: [SWord] -> [SWord]
-allPartOfInvalid words = L.filter anyInvalid words
-    where anyInvalid = any (\(SChar _ v _) -> v == Invalid)
+processScrabbleBoard :: Matrix SChar -> S.Set String -> Matrix SChar
+processScrabbleBoard board dictionary = validatedMatrix
+    where
+        --matrixToReturn = updateWithSChar board $ concat partOfInvalid
+        --partOfInvalid = updatePartOfInvalid . allPartOfInvalid . allWords $ traceShow validatedMatrix validatedMatrix
+        validatedMatrix = updateWithSChar' placedSinglesMx . concat $ valid ++ invalid
+        placedSinglesMx = foldr (\sc@(SChar l v p@(p1,p2)) mx -> Mx.setElem sc (position sc) mx) SB.empty $ concat single
+        xx@(valid, invalid, single) = updateSValid checked
+        checked = validatedWords board dictionary 
 
-updateSValid :: ([SWord], [SWord]) -> ([SWord], [SWord])
-updateSValid (valid, invalid) = (newValid, newInvalid)
+-- -- | Updates letters validness for words containing invalid letters.
+-- updatePartOfInvalid :: [SWord] -> [SWord]
+-- updatePartOfInvalid words = map mapPart words
+--     where mapPart = map updateIfValid
+--           updateIfValid (SChar c Valid p) = SChar c PartOfInvalid p 
+--           updateIfValid schar = schar
+
+-- -- | Returns words that contains any invalid letter.
+-- allPartOfInvalid :: [SWord] -> [SWord]
+-- allPartOfInvalid words = L.filter anyInvalid words
+--     where anyInvalid = any (\(SChar _ v _) -> v == Invalid)
+
+-- | Takes triple of validated words lists.
+--   Updates ScrabbleBoard characters with info about validness.
+updateSValid :: ([SWord], [SWord], [SWord]) -> ([SWord], [SWord], [SWord])
+updateSValid (valid, invalid, single) = (newValid, newInvalid, newSingle)
     where newValid = map (map $ updateValid Valid) valid
           newInvalid = map (map $ updateValid Invalid) invalid
+          newSingle = map (map $ updateValid SingleLetter) single
           updateValid status (SChar c _ pos) = SChar c status pos
 
-allValidatedWords :: Matrix SChar -> S.Set String -> ([SWord], [SWord])
-allValidatedWords mx dict = L.partition (\a -> S.member (word a) dict) $ allWords mx
+-- | Return triple of words lists (Valid, Invalid, SingleLetters) validated using dictionary.  
+validatedWords :: Matrix SChar -> S.Set String -> ([SWord], [SWord], [SWord])
+validatedWords mx dict = (newValid, newInvalid, single)
+    where 
+        newValid = valid
+        (newInvalid, single) = L.partition (\a -> L.length a > 1) invalid
+        (valid, invalid) = L.partition (\a -> S.member (word a) dict) $ allWords mx
 
-validWords :: Matrix SChar -> S.Set String -> [SWord]
-validWords mx dictionary = L.filter (\a -> S.member (word a) dictionary ) $ allWords mx
-
+-- | Returns all detected words
 allWords :: Matrix SChar -> [SWord]
 allWords mx = concat $ hWords ++ vWords
     where hWords = map sWords . Mx.toLists $ mx
           vWords = map sWords . Mx.toLists . Mx.transpose $ mx
 
+-- | This function is similar to Data.List.words but accept list of type SChar.
+--   Returns list of words.
 sWords :: [SChar] -> [SWord]
 sWords list = sWords' [] list []
 

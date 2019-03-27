d2ffd3215b4ebf95b11e34f0ceef781d6c272b73
Stanisław Wasik <st.wasik@gmail.com>
2019-03-27 23:01:13 +0100

    Added test values (scrabble board)

--- CheckerService/src/STest.hs
+++ CheckerService/src/STest.hs
@@ -6,10 +6,25 @@ import SData.SBoard as SB
 
 import Data.Matrix as Mx
 
-testWords x = map (map letter) $ concat $ map sWords $ toLists x
-
 alaMaKota = "ala ma kota kot ma ale 123 234 345 445  1234567890-=="
 
 asLettersMx x = Mx.fromLists $ (map $ map letter) $ Mx.toLists x
 
 x = SB.fromList $ alaMaKota ++ repeat ' '
+
+--           0123456789ABCDE
+testList =  "ewa ma kota    " ++
+            "w              " ++
+            "a              " ++
+            "       k       " ++
+            "       o       " ++
+            "       t       " ++
+            "      pies     " ++
+            "               " ++
+            "      piesek   " ++
+            "           o   " ++
+            "     kotek t   " ++
+            "      k        " ++
+            " r    o        " ++
+            "   r           " ++
+            "     t         "

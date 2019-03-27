10c0c36f1ed171ccc2b2f3c3be0ff596718dbb02
Stanisław Wasik <st.wasik@gmail.com>
2019-03-27 23:03:12 +0100

    Added setting proper SValid value in board, fixed matrix index bug

--- CheckerService/src/SData/SBoard.hs
+++ CheckerService/src/SData/SBoard.hs
@@ -5,9 +5,10 @@ import SData.SChar
 import Data.Tuple
 
 fromList :: [Char] -> Matrix SChar
-fromList charList = Mx.fromList 15 15 . map sChar . zip charList $ revTuple <$> [1..15] <*> [1..15]
-    where revTuple a b = swap (a,b)
+fromList charList = Mx.fromList 15 15 . map sChar . zip charList' $ tuple <$> [1..15] <*> [1..15]
+    where tuple a b = (a,b)
           sChar (char, position) = SChar char NotSet position
+          charList' = charList ++ repeat ' '
 
 empty :: Matrix SChar
 empty = SData.SBoard.fromList $ repeat ' '
@@ -17,3 +18,30 @@ updateWithSChar matrix [] = matrix
 updateWithSChar matrix (sc : scs) = updateWithSChar newMatrix scs
     where newMatrix = Mx.setElem sc pos matrix
           pos = position sc 
+
+
+updateWithSChar' :: Matrix SChar -> [SChar] -> Matrix SChar
+updateWithSChar' matrix [] = matrix
+updateWithSChar' matrix (sc : scs) = updateWithSChar' newMatrix scs
+    where
+        newMatrix = setProperValidness sc matrix
+        pos = position sc
+
+--setProperValidness (SChar l v p) matrix 
+setProperValidness sc@(SChar l Valid p@(p1,p2)) matrix 
+        | prev == NotSet = set sc
+        | prev == Valid = matrix 
+        | prev == PartOfInvalid = matrix
+        | prev == Invalid = set $ SChar l PartOfInvalid p
+        | prev == SingleLetter = set sc
+        where prev = valid $ Mx.getElem p1 p2 matrix
+              set sc = Mx.setElem sc (position sc) matrix
+
+setProperValidness sc@(SChar l Invalid p@(p1,p2)) matrix 
+        | prev == NotSet = set sc
+        | prev == Valid = set $ SChar l PartOfInvalid p
+        | prev == PartOfInvalid = matrix
+        | prev == Invalid = matrix 
+        | prev == SingleLetter = set sc 
+        where prev = valid $ Mx.getElem p1 p2 matrix
+              set sc = Mx.setElem sc (position sc) matrix

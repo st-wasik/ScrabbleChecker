69d3d7b5f7dbb785dc44c79cc7a1fe88d0e25ed4
Stanisław Wasik <st.wasik@gmai.com>
2019-03-30 23:37:17 +0100

    TGUI configuration added

--- ScrabbleCzeker/ScrabbleCzeker/Main.cpp
+++ ScrabbleCzeker/ScrabbleCzeker/Main.cpp
@@ -1,12 +1,15 @@
 #include "Main.h"
 
 #include <SFML/Graphics.hpp>
+#include <TGUI/TGUI.hpp>
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
+
+	tgui::Gui gui( window );
 
 	while (window.isOpen())
 	{
@@ -15,10 +18,13 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+
+			gui.handleEvent(event);
 		}
 
 		window.clear();
 		window.draw(shape);
+		gui.draw();
 		window.display();
 	}
 

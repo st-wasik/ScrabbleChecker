9aaec6d538f2e4dc8d3ac704aabdd8ebcdd0e766
Qroshi <szymon1804@wp.pl>
2019-04-01 12:31:12 +0200

    Board part 1

--- ScrabbleCzeker/ScrabbleCzeker/Main.cpp
+++ ScrabbleCzeker/ScrabbleCzeker/Main.cpp
@@ -1,11 +1,36 @@
 #include "Main.h"
-
 #include <SFML/Graphics.hpp>
 #include <TGUI/TGUI.hpp>
+#include <vector>
+
+void buildBoard(sf::RenderWindow &window)
+{
+	std::vector<std::vector<int>> board =
+	{
+		{2,0,0,3,0,0,0,2,0,0,0,3,0,0,2},
+		{0,1,0,0,0,4,0,0,0,4,0,0,0,1,0},
+		{0,0,1,0,0,0,3,0,3,0,0,0,1,0,0},
+		{0,0,0,1,0,0,0,3,0,0,0,1,0,0,0},
+		{0,0,0,0,1,0,0,0,0,0,1,0,0,0,0},
+		{0,4,0,0,0,4,0,0,0,4,0,0,0,4,0},
+		{0,0,3,0,0,0,3,0,3,0,0,0,3,0,0},
+		{2,0,0,3,0,0,0,5,0,0,0,3,0,0,2},
+		{0,0,3,0,0,0,3,0,3,0,0,0,3,0,0},
+		{0,4,0,0,0,4,0,0,0,4,0,0,0,4,0},
+		{0,0,0,0,1,0,0,0,0,0,1,0,0,0,0},
+		{0,0,0,1,0,0,0,3,0,0,0,1,0,0,0},
+		{0,0,1,0,0,0,3,0,3,0,0,0,1,0,0},
+		{0,1,0,0,0,4,0,0,0,4,0,0,0,1,0},
+		{2,0,0,3,0,0,0,2,0,0,0,3,0,0,2}
+	};
+	sf::RectangleShape background(sf::Vector2f(1280, 1280));
+	background.setFillColor(sf::Color::White);
+	window.draw(background);
+}
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(1280, 1280), "Scrable Czeker");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
 
@@ -21,8 +46,8 @@ int main()
 
 			gui.handleEvent(event);
 		}
-
 		window.clear();
+		buildBoard(window);
 		window.draw(shape);
 		gui.draw();
 		window.display();

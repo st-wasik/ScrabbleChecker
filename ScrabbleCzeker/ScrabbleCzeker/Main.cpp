1496543e9bfd97869110135f7703d8f74b6bc631
Qroshi <szymon1804@wp.pl>
2019-04-01 13:29:23 +0200

    Board part 2 - different fields and addBlank method added

--- ScrabbleCzeker/ScrabbleCzeker/Main.cpp
+++ ScrabbleCzeker/ScrabbleCzeker/Main.cpp
@@ -23,16 +23,48 @@ void buildBoard(sf::RenderWindow &window)
 		{0,1,0,0,0,4,0,0,0,4,0,0,0,1,0},
 		{2,0,0,3,0,0,0,2,0,0,0,3,0,0,2}
 	};
-	sf::RectangleShape background(sf::Vector2f(1280, 1280));
+	sf::RectangleShape background(sf::Vector2f(980, 980));
 	background.setFillColor(sf::Color::White);
 	window.draw(background);
+	for (int i = 0; i < board.size(); i++)
+	{
+		for (int j = 0; j < board[i].size(); j++)
+		{
+			sf::RectangleShape field(sf::Vector2f(60, 60));
+			field.setPosition((1 + j) * 5 + j * 60, (1 + i) * 5 + i * 60);
+			if (board[i][j] == 0)
+			field.setFillColor(sf::Color::Green);
+			if (board[i][j] == 1)
+			field.setFillColor(sf::Color::Yellow);
+			if (board[i][j] == 2)
+			field.setFillColor(sf::Color::Red);
+			if (board[i][j] == 3)
+			field.setFillColor(sf::Color::Cyan);
+			if (board[i][j] == 4)
+			field.setFillColor(sf::Color::Blue);
+			if (board[i][j] == 5)
+			field.setFillColor(sf::Color::Magenta);
+			window.draw(field);
+		}
+	}
+}
+
+void addBlank(tgui::Gui &gui, std::string letter, int x, int y)
+{
+	tgui::Theme theme{ "C:/Users/HP/Desktop/projekt_z/ScrabbleChecker/ScrabbleCzeker/TGUI-0.8/themes/TransparentGrey.txt" };
+	auto blank = tgui::Button::create();
+	blank->setRenderer(theme.getRenderer("Button"));
+	blank->setPosition((1 + y) * 5 + y * 60 + 3, (1 + x) * 5 + x * 60 + 3);
+	blank->setText(letter);
+	blank->setTextSize(38);
+	blank->setSize(54, 54);
+	blank->setEnabled(0);
+	gui.add(blank);
 }
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(1280, 1280), "Scrable Czeker");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(980, 980), "Scrable Czeker");
 
 	tgui::Gui gui( window );
 
@@ -48,7 +80,9 @@ int main()
 		}
 		window.clear();
 		buildBoard(window);
-		window.draw(shape);
+		addBlank(gui, "K", 0, 0);
+		addBlank(gui, "O", 0, 1);
+		addBlank(gui, "T", 0, 2);
 		gui.draw();
 		window.display();
 	}

96b32c6d46b50c8d232b3cd5ea4c5e614a6ca0e0
Stanisław Wasik <st.wasik@gmai.com>
2019-04-02 11:57:46 +0200

    Added Application class

---
+++ ScrabbleCzeker/ScrabbleCzeker/Application.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <TGUI/TGUI.hpp>
+class Application
+{
+public:
+	Application();
+	~Application();
+	void run();
+private:
+	sf::RenderWindow window;
+
+	sf::Texture t, star;
+	std::vector<sf::RectangleShape> shapes;
+
+	tgui::Gui gui;
+	tgui::Theme theme;
+
+	tgui::Font font;
+
+	void addTile(std::wstring letter, int x, int y);
+
+	std::vector<sf::RectangleShape> buildBoard();
+};
+

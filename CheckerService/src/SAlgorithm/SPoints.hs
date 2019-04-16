83ea652c33e3cdd45277d3adde2054210669d794
Stanisław Wasik <st.wasik@gmail.com>
2019-04-16 21:42:57 +0200

    Added point-count module

---
+++ CheckerService/src/SAlgorithm/SPoints.hs
@@ -0,0 +1,43 @@
+module SAlgorithm.SPoints(wordValue) where
+
+import SData.SChar
+
+wordValue :: SWord -> Int
+wordValue word = chars * wordExtra
+    where
+        chars = sum $ map (charValue) word
+        wordExtra = product $ map (charPosWordFactor) word
+
+charValue :: SChar -> Int
+charValue char = charPoints char * charPosFactor char
+
+charPoints :: SChar -> Int
+charPoints char
+    | (letter char) `elem` ['a','e','i','n','o','r','s','w','z'] = 1
+    | (letter char) `elem` ['c','d','k','l','m','p','t','y'] = 2
+    | (letter char) `elem` ['b','g','h','j','u', '\321'] = 3 -- Ł
+    | (letter char) `elem` ['f','\261','\281','\243','\347','\380'] = 5 -- Ą, Ę, Ó, Ś, Ż
+    | (letter char) == '\263' = 6 --Ć
+    | (letter char) == '\324' = 6 --Ń
+    | (letter char) == '\378' = 6 --Ź
+    | otherwise = 0
+
+charPosFactor :: SChar -> Int
+charPosFactor char 
+    | (position char) `elem` [(1,4), (1,12), (3,7), (3,8), (3,9)] = 2
+    | (position char) `elem` [(15,4),(15,12),(13,7),(13,8),(13,9)] = 2
+    | (position char) `elem` [(4,15),(12,15),(7,13),(8,13),(9,13)] = 2
+    | (position char) `elem` [(4,1),(12,1),(7,3),(8,3),(9,3)] = 2
+    | (position char) `elem` [(7,7),(7,9),(9,7),(9,9)] = 2
+    | (position char) `elem` [(2,6),(2,10),(6,2),(10,2),(6,14),(10,14),(14,6),(14,10)] = 3
+    | (position char) `elem` [(6,6),(6,10),(10,6),(10,10)] = 3
+    | otherwise = 1
+
+charPosWordFactor :: SChar -> Int
+charPosWordFactor char 
+    | (position char) `elem` [(2,2),(3,3),(4,4),(5,5)] = 2
+    | (position char) `elem` [(11,11),(12,12),(13,13),(14,14)] = 2
+    | (position char) `elem` [(14,2),(13,3),(12,4),(11,5)] = 2
+    | (position char) `elem` [(5,11),(4,12),(3,13),(2,14)] = 2
+    | (position char) `elem` [(1,1), (8,1), (15,1), (1,8), (15,8), (1,15), (8,15), (15,15)] = 3
+    | otherwise = 1
